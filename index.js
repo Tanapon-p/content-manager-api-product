94777db3e1bc4e90aff286b83ff2cb9e0451522c
Benz <t.poonsanchai@gmail.com>
2023-11-13 17:10:24 +0700

    Specifies port from env variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 const fs = require("fs");
 const path = require("path");
 const pathToFile = path.resolve("./data.json");
